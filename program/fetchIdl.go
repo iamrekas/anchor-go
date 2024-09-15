fc5df571d74ad8830e236289602bcb8d35299112
rekas <aure369@gmail.com>
2024-09-15 17:44:34 +0300

    add fetch idl

---
+++ program/fetchIdl.go
@@ -0,0 +1,28 @@
+package program
+
+import (
+	"bytes"
+	"compress/zlib"
+	"context"
+	"io/ioutil"
+
+	"github.com/gagliardetto/solana-go"
+	"github.com/gagliardetto/solana-go/rpc"
+)
+
+func FetchIDL(ctx context.Context, rpc *rpc.Client, address solana.PublicKey) ([]byte, error) {
+	var accInfo IdlProgramAccount
+	err := rpc.GetAccountDataBorshInto(ctx, address, &accInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	//unCompress
+	vv, err := zlib.NewReader(bytes.NewReader(accInfo.Data))
+	if err != nil {
+		return nil, err
+	}
+	defer vv.Close()
+	bf, err := ioutil.ReadAll(vv)
+	return bf, err
+}
